188a0208915d1486e5b70b913fa5ca7fc0b20665
Berk Dağlı <ynsuexy9483_aehit@students.vizja.pl>
2026-02-11 03:12:56 +0100

    added right click ignore

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+if (import.meta.env.PROD) {
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
